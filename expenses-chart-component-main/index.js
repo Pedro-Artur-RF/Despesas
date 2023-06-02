1f4d015a31b5a3df9d03fa6a79febb961e710b13
Pedro Artur (Motsu) <71746213+Pedro-Artur-RF@users.noreply.github.com>
2023-06-02 09:40:41 -0300

    1st commit

---
+++ expenses-chart-component-main/index.js
@@ -0,0 +1,43 @@
+let data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ];
+
+  let  mon = document.getElementById('mon-bar');
+  let  tue = document.getElementById('tue-bar'); 
+  let  wed = document.getElementById('wed-bar'); 
+  let  thu = document.getElementById('thu-bar'); 
+  let  fri = document.getElementById('fri-bar'); 
+  let  sat = document.getElementById('sat-bar'); 
+  let  sun = document.getElementById('sun-bar');
+
+  function defHeight(day, height){
+    
+  }
+
